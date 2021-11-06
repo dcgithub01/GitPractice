4d036ee85fe9af49ab664823a1adced31c7a3e6d
Deepak Chhabra <deepak.chhabra01@gmail.com>
2021-11-06 20:28:39 +0530

    first commit

---
+++ src/pages/LogInPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class LogInPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Login pages");
+	}
+
+}
---
+++ src/tests/HomeTests.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class HomeTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
