2b49dc3fbfb72e7839b83bacfca595d2af934aec
Deepak Chhabra <deepak.chhabra01@gmail.com>
2021-11-07 11:35:02 +0530

    used new variable for login

--- src/pages/LogInPage.java
+++ src/pages/LogInPage.java
@@ -5,6 +5,9 @@ public class LogInPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Login pages");
+		String dc="new value of dcccc";
+		int percent = 5 ;
+		
 	}
 
 }
