67c0ccbaf49265ca6a50c974a28282227225d16c
Deepak Chhabra <deepak.chhabra01@gmail.com>
2021-11-07 13:48:37 +0530

    added new variables

--- src/pages/LogInPage.java
+++ src/pages/LogInPage.java
@@ -5,6 +5,9 @@ public class LogInPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Login pages");
+		int rate =5;
+		String user="HERO";
+		
 	}
 
 }
