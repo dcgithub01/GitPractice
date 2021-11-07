56f2777b2e0be794b4fd78e8843d16e371cd41d2
Deepak Chhabra <deepak.chhabra01@gmail.com>
2021-11-07 08:58:10 +0530

    added reg page and mytest

---
+++ src/pages/RegPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class RegPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("RegPages");
+int i =10;
+	}
+
+}
---
+++ src/tests/MyTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my test");
+
+	}
+
+}
