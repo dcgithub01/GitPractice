26855260ae7bb89653cb689505f12be0398eba33
Deepak Chhabra <deepak.chhabra01@gmail.com>
2021-11-07 09:08:44 +0530

    added string variable dc

--- src/tests/MyTest.java
+++ src/tests/MyTest.java
@@ -5,6 +5,7 @@ public class MyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my test");
+		String dc="secondlast change";
 
 	}
 
