d7c1d8a03b4f6a8541664f56c85567fb73dbb450
Deepak Chhabra <deepak.chhabra01@gmail.com>
2021-11-07 07:18:02 +0530

    created,modified and deleted files

--- src/pages/SearchPage.java
+++ src/pages/SearchPage.java
@@ -1,12 +1,11 @@
 package pages;
 
-public class HomePage {
+public class SearchPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HomePage");
-		int age =33;
-		String name="Deepak";
+
+		System.out.println("this is search page coding ");
 	}
 
 }
